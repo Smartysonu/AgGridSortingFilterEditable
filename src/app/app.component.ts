a1746610fc3d9c16b5115e48fb196457bc3ecd82
sonal <sonalpriya655@gmail.com>
2021-10-30 17:15:54 +0530

    Aggrid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'grid';
+
+  columnDefs=[
+    { headerName : "Name", field: "name",width:150, sortable: true, filter: true, editable :true},
+    { headerName : "Age", field: "age",width:150, sortable:true, filter: true, editable :true},
+    { headerName : "Location", field: "location",width:150, sortable:true, filter: true, editable :true},
+    { headerName : "Gender", field: "gender",width:150, sortable:true, filter: true, editable :true}
+  ];
+  rowData=[
+    { name: 'Sonal', age: 6, gender: 32 , location: 'Patna'},
+    { name: 'Sonu', age: 6, gender: 37 , location: 'Pune'},
+    { name: 'Priya', age: 6 , gender: 34, location: 'Bihar'},
+    { name: 'Aggrid', age: 6 ,gender: 26, location: 'Maharastra', }
+  ];
 }
